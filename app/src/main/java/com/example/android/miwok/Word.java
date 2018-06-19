a5868dd37a0cfee2f67231ad7ab84943a4571c55
RalphM <36464444+lakerfan32@users.noreply.github.com>
2018-06-18 22:27:29 -0400

    -Updated Word and WordAdapter classes so that ImageVIew is shown depending on whether list item requires it or not

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -18,7 +18,12 @@ public class Word {
     /**
      * Image resource ID for the word
      */
-    private int mImageResourceId;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    /**
+     * Constant value that represents no image was provided for this word
+     */
+    private static final int NO_IMAGE_PROVIDED = -1;
 
     /**
      * Create a new Word object.
@@ -65,6 +70,14 @@ public class Word {
       */
       public int getImageResourceId()  {
           return mImageResourceId;
+      }
+
+    /**
+     * Returns whether or not there is an image for this word.
+     * @return
+     */
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
       }
 
 }
